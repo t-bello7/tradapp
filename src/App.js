f191b06e206a2ce31997429f761ac927a7608922
Bell Oluwatomisin <bello4aus@gmail.com>
2021-11-21 10:42:30 +0100

    App.js: removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Detail from './Pages/Detail';
 import Home from './Pages/Home';
